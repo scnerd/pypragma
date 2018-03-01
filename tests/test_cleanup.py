65aeab0a536da69f80d975dd95543d68eb1b8cf1
dmaxson <dmaxson@ltsnet.net>
2018-03-01 14:58:39 -0500

    Uncommented cleanup code to start working on it again

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -1,31 +1,31 @@
-# from textwrap import dedent
-#
-# import pragma
-# from .test_pragma import PragmaTest
-#
-#
-# class TestCleanup(PragmaTest):
-#     def test_basic_assign(self):
-#         @pragma.cleanup(return_source=True)
-#         def f():
-#             x = 5
-#             return 3
-#
-#         result = dedent('''
-#         def f():
-#             return 3
-#         ''')
-#         self.assertEqual(f.strip(), result.strip())
-#
-#     def test_retrieval(self):
-#         @pragma.cleanup(return_source=True)
-#         @pragma.collapse_literals
-#         def f():
-#             x = 5
-#             return x
-#
-#         result = dedent('''
-#         def f():
-#             return 5
-#         ''')
-#         self.assertEqual(f.strip(), result.strip())
+from textwrap import dedent
+
+import pragma
+from .test_pragma import PragmaTest
+
+
+class TestCleanup(PragmaTest):
+    def test_basic_assign(self):
+        @pragma.cleanup(return_source=True)
+        def f():
+            x = 5
+            return 3
+
+        result = dedent('''
+        def f():
+            return 3
+        ''')
+        self.assertEqual(f.strip(), result.strip())
+
+    def test_retrieval(self):
+        @pragma.cleanup(return_source=True)
+        @pragma.collapse_literals
+        def f():
+            x = 5
+            return x
+
+        result = dedent('''
+        def f():
+            return 5
+        ''')
+        self.assertEqual(f.strip(), result.strip())
