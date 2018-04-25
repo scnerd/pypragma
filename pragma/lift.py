ec2332f3079d751d033107cb3bffe2a3576f9735
scnerd <scnerd@gmail.com>
2018-04-25 12:41:19 -0400

    Converted lambda lift into a class to make it more intelligible

--- pragma/lift.py
+++ pragma/lift.py
@@ -19,28 +19,78 @@ else:
 
 
 @optional_argument_decorator
-@magic_contract
-def lift(return_source=False, save_source=True, annotate_types=False, defaults=False, lift_globals=None, imports=True):
-    """Converts a closure or method into a pure function which accepts locally defined variables as keyword arguments
+class lift:
+    @magic_contract
+    def __init__(self, return_source=False, save_source=True, annotate_types=False, defaults=False, lift_globals=None, imports=True):
+        """Converts a closure or method into a pure function which accepts locally defined variables as keyword arguments
 
-    :param return_source: Returns the transformed function's source code instead of compiling it
-    :type return_source: bool
-    :param save_source: Saves the function source code to a tempfile to make it inspectable
-    :type save_source: bool
-    :param annotate_types: Flag (or list of var names, or mapping) to use the types of closure variables as the type annotation of the keyword arguments
-    :type annotate_types: bool|list|set|tuple|dict
-    :param defaults: Default values for free arguments. Must be a dictionary of literals or AST expression, or a bool in which case a best effort is made to convert the closure values into default values
-    :type defaults: bool|list|set|tuple|dict
-    :param lift_globals: List of global variables to lift to keyword arguments
-    :type lift_globals: None|list|set|tuple
-    :param imports: Flag or list of imports to include within the function body
-    :type imports: bool|list|set|tuple
-    :return: The transformed function, or its source code if requested
-    :rtype: Callable
-    """
+        :param return_source: Returns the transformed function's source code instead of compiling it
+        :type return_source: bool
+        :param save_source: Saves the function source code to a tempfile to make it inspectable
+        :type save_source: bool
+        :param annotate_types: Flag (or list of var names, or mapping) to use the types of closure variables as the type annotation of the keyword arguments
+        :type annotate_types: bool|list|set|tuple|dict
+        :param defaults: Default values for free arguments. Must be a dictionary of literals or AST expression, or a bool in which case a best effort is made to convert the closure values into default values
+        :type defaults: bool|list|set|tuple|dict
+        :param lift_globals: List of global variables to lift to keyword arguments
+        :type lift_globals: None|list|set|tuple
+        :param imports: Flag or list of imports to include within the function body
+        :type imports: bool|list|set|tuple
+        """
+
+        self.return_source = return_source
+        self.save_source = save_source
+        self.annotate_types = annotate_types
+        self.defaults = defaults
+        self.lift_globals = lift_globals
+        self.imports = imports
+
+    def annotate(self, k, v):
+        if self.annotate_types:
+            if isinstance(self.annotate_types, bool):
+                return ast.Name(id=type(v).__name__, ctx=ast.Load())
+            elif isinstance(self.annotate_types, (tuple, list, set)):
+                if k in self.annotate_types:
+                    return ast.Name(id=type(v).__name__, ctx=ast.Load())
+            elif isinstance(self.annotate_types, dict):
+                if k in self.annotate_types:
+                    result = self.annotate_types[k]
+                    if isinstance(result, str):
+                        result = ast.Str(s=result)
+                    if result and not isinstance(result, ast.expr):
+                        raise TypeError("Type annotation must be a string or AST expression (got {})".format(result))
+                    return result
+        return None
+
+    def get_default(self, k, v):
+        if self.defaults:
+            if isinstance(self.defaults, bool):
+                attempt = v
+            elif isinstance(self.defaults, (tuple, list, set)):
+                if k in self.defaults:
+                    attempt = v
+                else:
+                    return None
+            elif isinstance(self.defaults, dict):
+                if k in self.defaults:
+                    attempt = self.defaults[k]
+                else:
+                    return None
+            else:
+                return None
+
+            try:
+                res = make_ast_from_literal(attempt)
+                assert isinstance(res, ast.expr)
+                return res
+            except (TypeError, AssertionError):
+                log.debug("Failed to convert {} to an AST expression".format(attempt))
+                return None
+
+        return None
 
     @magic_contract(f='Callable', returns='Callable|str')
-    def inner(f):
+    def __call__(self, f):
         f_mod, f_body, f_file = function_ast(f)
         # Grab function closure variables
         add_imports = []
@@ -50,10 +100,10 @@ def lift(return_source=False, save_source=True, annotate_types=False, defaults=F
         else:
             free_vars = []
 
-        for glbl in lift_globals or []:
+        for glbl in self.lift_globals or []:
             free_vars.append((glbl, f.__globals__[glbl]))
 
-        if imports:
+        if self.imports:
             for k, v in f.__globals__.items():
                 if isinstance(v, ModuleType):
                     add_imports.append((k, v))
@@ -75,57 +125,13 @@ def lift(return_source=False, save_source=True, annotate_types=False, defaults=F
             f_body = f_docstring + [
                 ast.Import(names=[ast.alias(name=v.__name__, asname=k if k != v.__name__ else None)])
                 for k, v in add_imports
-                if (isinstance(imports, bool) or k in imports) and k not in _exclude
+                if (isinstance(self.imports, bool) or k in self.imports) and k not in _exclude
             ] + f_body
 
         func_def = f_mod.body[0]
 
-        def annotate(k, v):
-            if annotate_types:
-                if isinstance(annotate_types, bool):
-                    return ast.Name(id=type(v).__name__, ctx=ast.Load())
-                elif isinstance(annotate_types, (tuple, list, set)):
-                    if k in annotate_types:
-                        return ast.Name(id=type(v).__name__, ctx=ast.Load())
-                elif isinstance(annotate_types, dict):
-                    if k in annotate_types:
-                        result = annotate_types[k]
-                        if isinstance(result, str):
-                            result = ast.Str(s=result)
-                        if result and not isinstance(result, ast.expr):
-                            raise TypeError("Type annotation must be a string or AST expression (got {})".format(result))
-                        return result
-            return None
-
-        def get_default(k, v):
-            if defaults:
-                if isinstance(defaults, bool):
-                    attempt = v
-                elif isinstance(defaults, (tuple, list, set)):
-                    if k in defaults:
-                        attempt = v
-                    else:
-                        return None
-                elif isinstance(defaults, dict):
-                    if k in defaults:
-                        attempt = defaults[k]
-                    else:
-                        return None
-                else:
-                    return None
-
-                try:
-                    res = make_ast_from_literal(attempt)
-                    assert isinstance(res, ast.expr)
-                    return res
-                except (TypeError, AssertionError):
-                    log.debug("Failed to convert {} to an AST expression".format(attempt))
-                    return None
-
-            return None
-
-        new_kws = [ast.arg(arg=k, annotation=annotate(k, v)) for k, v in free_vars]
-        new_kw_defaults = [get_default(k, v) for k, v in free_vars]
+        new_kws = [ast.arg(arg=k, annotation=self.annotate(k, v)) for k, v in free_vars]
+        new_kw_defaults = [self.get_default(k, v) for k, v in free_vars]
 
         new_func_def = ast.FunctionDef(
             name=func_def.name,
@@ -144,7 +150,7 @@ def lift(return_source=False, save_source=True, annotate_types=False, defaults=F
 
         f_mod.body[0] = new_func_def
 
-        if return_source or save_source:
+        if self.return_source or self.save_source:
             try:
                 source = astor.to_source(f_mod)
             except ImportError:  # pragma: nocover
@@ -155,20 +161,18 @@ def lift(return_source=False, save_source=True, annotate_types=False, defaults=F
         else:
             source = None
 
-        if return_source:
+        if self.return_source:
             return source
         else:
             f_mod = ast.fix_missing_locations(f_mod)
-            if save_source:
+            if self.save_source:
                 temp = tempfile.NamedTemporaryFile('w', delete=True)
                 f_file = temp.name
             no_globals = {}
             exec(compile(f_mod, f_file, 'exec'), no_globals)
             func = no_globals[f_mod.body[0].name]
-            if save_source:
+            if self.save_source:
                 func.__tempfile__ = temp
                 temp.write(source)
                 temp.flush()
             return func
-
-    return inner
