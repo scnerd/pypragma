fd559490033508ebe0543fb4d778218ec6417628
Alex Tait <alexander.tait@nist.gov>
2020-12-26 23:13:11 -0700

    version bump 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pragma',
-    version='0.2.2',
+    version='0.2.3',
     packages=['pragma', 'pragma.core', 'pragma.core.resolve'],
     url='https://github.com/scnerd/pypragma',
     license='MIT',
